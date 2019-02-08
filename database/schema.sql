de89208f56b1f926db0f4cca9c8ab83eccbeb907
Anthony Burroughs <bonfilio.tony@gmail.com>
2019-02-08 15:30:45 -0600

    Seeds 100 rows of data in database from mockdata.js

--- database/schema.sql
+++ database/schema.sql
@@ -5,6 +5,7 @@ USE fec;
 CREATE TABLE products (
   product_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   product_name VARCHAR(500) NOT NULL,
+  product_description VARCHAR(500) NOT NULL,
   image_src VARCHAR(500) NOT NULL,
   review_stars INT NOT NULL, 
   review_count INT NOT NULL,
--- database/seed.js
+++ database/seed.js
@@ -3,7 +3,7 @@ const db = require('./index.js');
 console.log('data is ', data);
 
 data.forEach(product=> {
-    let queryStr = `INSERT INTO products (product_name, image_src, review_stars, review_count, price) VALUES ('${product.product_name}','${product.image_src}', '${product.review_stars}', '${product.review_count}', '${product.price}');`;
+    let queryStr = `INSERT INTO products (product_name, product_description, image_src, review_stars, review_count, price) VALUES ("${product.product_name}","${product.product_description}","${product.image_src}", "${product.review_stars}", "${product.review_count}", "${product.price}");`;
     db.query(queryStr, (err, result) => {
       if(err) {
           console.log('Error with insertion into database, err is: ', err)
